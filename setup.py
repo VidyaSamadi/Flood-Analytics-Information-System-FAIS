d1cbde5d1e4dd9de31cc95cce1333efe9aaeeab1
NATTAPON DONRATANAPAT <pleuk5667@gmail.com>
2019-02-27 18:40:30 -0500

    init change

--- setup.py
+++ setup.py
@@ -4,15 +4,14 @@ with open("README.md" , "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='fsia',
-    version='0.1.1',
-    scripts=['fsia'],
+     name="fsia",
+    version="0.0.1",
     author="Nattapon Donratanapat",
     author_email="pleuk5667@gmail.com",
-    description="A data gathering tools for USGS and Twitter",
+    description="USGS and Twitter data gathering and analysis tools",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/VidyaSamadi/Research-Team",
+    url="https://github.com/VidyaSamadi/Research-Team-private",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
