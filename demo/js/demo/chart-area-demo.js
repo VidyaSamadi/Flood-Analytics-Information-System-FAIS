444e78b5b52890028791942f404a00da40ffb771
NATTAPON DONRATANAPAT <pleuk5667@gmail.com>
2019-03-19 20:28:13 -0400

    update visual

--- demo/js/demo/chart-area-demo.js
+++ demo/js/demo/chart-area-demo.js
@@ -32,7 +32,7 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    labels: ["3/1/2019", "3/2/2019", "3/3/2019", "3/4/2019", "3/5/2019"],
     datasets: [{
       label: "Earnings",
       lineTension: 0.3,
@@ -46,7 +46,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+      data: [6.70, 7.47, ],
     }],
   },
   options: {
